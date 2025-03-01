d9e6343c4f0e0bea8500b7ad5e44b9f17819b183
comnk <vhsiao36@gmail.com>
2025-03-01 01:15:08 -0800

    added new command where bot will generate a story and write it to a PDF that the user can download and read later

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
-# bot.py
 import os
-
 import discord
 from openai import OpenAI
 from dotenv import load_dotenv
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,6 +15,33 @@ client = discord.Client(command_prefix='!', intents=intents)
 openai_client = OpenAI()
 openai_client.api_key = os.getenv('OPENAI_API_KEY')
 
+async def generate_story():
+    response = openai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": "Tell an engaging short story as if you're a parent telling it to a child."}
+        ]
+    )
+    return response.choices[0].message.content
+
+def create_pdf(story_text, filename="story.pdf"):
+    pdf_path = f"./{filename}"
+    c = canvas.Canvas(pdf_path, pagesize=letter)
+    c.setFont("Helvetica", 12)
+
+    y_position = 750
+    for line in story_text.split("\n"):
+        c.drawString(50, y_position, line)
+        y_position -= 20
+
+        if y_position < 50: 
+            c.showPage()
+            c.setFont("Helvetica", 12)
+            y_position = 750
+
+    c.save()
+    return pdf_path
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -25,14 +52,18 @@ async def on_message(message):
         return
 
     if message.content == "!story":
-        response = openai_client.chat.completions.create(
-        model="gpt-4o-mini",
-        store=True,
-        messages=[
-            {"role": "user", "content": "You are a parent telling the user a story. Tell the user an engaging story."}
-        ]
-        )
+        await message.channel.send("HELLO!")
 
-        await message.channel.send(response.choices[0].message.content)
+    if message.content == "!storypdf":
+        await message.channel.send("Generating a story and saving it as a PDF...")
+
+        story_text = await generate_story()
+
+        pdf_filename = f"{message.author.name}_story.pdf"
+        pdf_path = create_pdf(story_text, pdf_filename)
+
+        await message.channel.send(file=discord.File(pdf_path))
+
+        os.remove(pdf_path)
 
 client.run(TOKEN)
